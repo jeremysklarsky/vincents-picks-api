a04a7982a37ee7aaad1e3f69aa05c4eb7547e581
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-05-01 14:01:30 -0400

    can create new users via client

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,10 +2,30 @@ module Api
 
   class UsersController < Api::BaseController
 
+    # def update
+    #   user = current_user
+
+    #   if user.update(user_params)
+    #     render json: user, status: 200, location: [:api, user]
+    #   else
+    #     render json: { errors: user.errors }, status: 422
+    #   end
+    # end
+
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render :nothing => true, :status => 200
+      else
+        render json: get_resource.errors, status: :unprocessable_entity
+      end      
+    end
+
   private
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
   include Authenticable
+
+  before_action :authenticate_with_token!, only: [:update, :destroy]
+  respond_to :json
+
 end
